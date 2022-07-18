177453724dbf63a484cf93fbea99a141ff4f79dc
alisyabob <sjs939883@gmail.com>
2022-07-19 02:57:36 +0900

    [CHORE] 프로젝트 초기 환경 세팅

---
+++ src/main/java/com/example/healthfunctionalfood/HealthFunctionalFoodApplication.java
@@ -0,0 +1,13 @@
+package com.example.healthfunctionalfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthFunctionalFoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthFunctionalFoodApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/healthfunctionalfood/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.healthfunctionalfood.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class HomeController {
+
+    @GetMapping("/")
+    public void test (){
+        log.info("HomeController");
+    }
+}
