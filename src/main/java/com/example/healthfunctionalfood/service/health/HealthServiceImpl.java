9a84b8b598179adc93e4507841557b8ebc0af15d
alisyabob <sjs939883@gmail.com>
2022-07-27 19:55:21 +0900

    [Refactor] 코드 정리

--- src/main/java/com/example/healthfunctionalfood/service/health/HealthServiceImpl.java
+++ src/main/java/com/example/healthfunctionalfood/service/health/HealthServiceImpl.java
@@ -91,89 +91,88 @@ public class HealthServiceImpl implements HealthService{
         // 카카오 로그인으로 사용자 정보 받는 로직 추가 필요
         User user = userRepository.findById(1L).orElseThrow(
                 () -> new ApiRequestException("존재하지 않는 사용자입니다."));
+        Optional<Diagnosis> diagnosisOptional1 = diagnosisRepository.findByUserId(user.getId()).stream().findFirst();
+        if(diagnosisOptional1.isEmpty()){
+            HealthToken healthToken = healthTokenRepository.findByUserId(user.getId()).get();
 
-        HealthToken healthToken = healthTokenRepository.findByUserId(user.getId()).get();
+            String medication = tilkoService.getMedication(userInfo, healthToken);
 
-        String medication = tilkoService.getMedication(userInfo, healthToken);
+            JSONParser jsonParser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(medication);
+            JSONArray resultList = (JSONArray) jsonObject.get("ResultList");
+            for(int i = 0; i< resultList.size(); i++){
+                JSONObject object = (JSONObject) resultList.get(i);
+                String name = (String) object.get("ByungEuiwonYakGukMyung");    //병원명
+                String JinlyoGaesiil = (String) object.get("JinRyoGaesiIl");    //진료날짜
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+                LocalDateTime localDateTime = LocalDateTime.parse(JinlyoGaesiil+" 00:00:00", formatter);
+                String JinlyoHyeongtae = (String) object.get("JinRyoHyungTae");    //진료형태
+                String CheobangHoesu = String.valueOf(object.get("CheoBangHoiSoo"));//처방횟수
+                int BangmunIlsu = Integer.valueOf((String) object.get("BangMoonIpWonIlsoo"));   //방문일수
+                int TuyagIlsu = Integer.valueOf((String) object.get("TuYakYoYangHoiSoo"));    //투약일수
 
-        JSONParser jsonParser = new JSONParser();
-        JSONObject jsonObject = (JSONObject) jsonParser.parse(medication);
-        JSONArray resultList = (JSONArray) jsonObject.get("ResultList");
-        for(int i = 0; i< resultList.size(); i++){
-            JSONObject object = (JSONObject) resultList.get(i);
-            String name = (String) object.get("ByungEuiwonYakGukMyung");    //병원명
-            String JinlyoGaesiil = (String) object.get("JinRyoGaesiIl");    //진료날짜
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-            LocalDateTime localDateTime = LocalDateTime.parse(JinlyoGaesiil+" 00:00:00", formatter);
-            String JinlyoHyeongtae = (String) object.get("JinRyoHyungTae");    //진료형태
-            String CheobangHoesu = String.valueOf(object.get("CheoBangHoiSoo"));//처방횟수
-            int BangmunIlsu = Integer.valueOf((String) object.get("BangMoonIpWonIlsoo"));   //방문일수
-            int TuyagIlsu = Integer.valueOf((String) object.get("TuYakYoYangHoiSoo"));    //투약일수
-
-            List<Diagnosis> diagnosisList = new ArrayList<>();
-            JSONArray retrieveTreatmentInjectionInformationPersonDetailList = (JSONArray) object.get("RetrieveTreatmentInjectionInformationPersonDetailList");
+                List<Diagnosis> diagnosisList = new ArrayList<>();
+                JSONArray retrieveTreatmentInjectionInformationPersonDetailList = (JSONArray) object.get("RetrieveTreatmentInjectionInformationPersonDetailList");
 
 
-            for (int j =0; j<retrieveTreatmentInjectionInformationPersonDetailList.size(); j++){
-                Object RetrieveTreatmentInjectionInformationPersonDetailList = retrieveTreatmentInjectionInformationPersonDetailList.get(j);
+                for (int j =0; j<retrieveTreatmentInjectionInformationPersonDetailList.size(); j++){
+                    Object RetrieveTreatmentInjectionInformationPersonDetailList = retrieveTreatmentInjectionInformationPersonDetailList.get(j);
 
-                String RetrieveTreatmentInjectionInformationPersonDetailListStr = RetrieveTreatmentInjectionInformationPersonDetailList.toString();
-                JSONObject retrieveTreatment = (JSONObject) jsonParser.parse(RetrieveTreatmentInjectionInformationPersonDetailListStr);
-                retrieveTreatment.get("JinRyoHyungTae");
-                retrieveTreatment.get("ChoBangHoetSoo");
-                Object retrieveMdsupDtlInfo = retrieveTreatment.get("RetrieveMdsupDtlInfo");
-                String retrieveMdsupDtlInfoStr = retrieveMdsupDtlInfo.toString();
-                JSONObject retrieveMdsupDtlInfoObj = (JSONObject) jsonParser.parse(retrieveMdsupDtlInfoStr);
-                String drugCode = String.valueOf(retrieveMdsupDtlInfoObj.get("DrugCode"));
-                String mediPrdcNm = String.valueOf(retrieveMdsupDtlInfoObj.get("MediPrdcNm"));
-                String cmpnInfo = String.valueOf(retrieveMdsupDtlInfoObj.get("CmpnInfo"));
-                String tmsgGnlSpcd = String.valueOf(retrieveMdsupDtlInfoObj.get("TmsgGnlSpcd"));
-                String snglCmtnYn = String.valueOf(retrieveMdsupDtlInfoObj.get("SnglCmtnYn"));
-                String upsoName = String.valueOf(retrieveMdsupDtlInfoObj.get("UpsoName"));
-                String upso1 = String.valueOf(retrieveMdsupDtlInfoObj.get("Upso1"));
-                String fomlCdXplnCnte = String.valueOf(retrieveMdsupDtlInfoObj.get("FomlCdXplnCnte"));
-                String mdctPathXplnCnte = String.valueOf(retrieveMdsupDtlInfoObj.get("MdctPathXplnCnte"));
-                String mohwClsfNoXplnCnte = String.valueOf(retrieveMdsupDtlInfoObj.get("MohwClsfNoXplnCnte"));
-                String atcInfo = String.valueOf(retrieveMdsupDtlInfoObj.get("AtcInfo"));
-                String kpicInfo = String.valueOf(retrieveMdsupDtlInfoObj.get("KpicInfo"));
-                String efftEftCnte = String.valueOf(retrieveMdsupDtlInfoObj.get("EfftEftCnte"));
-                String usagCpctCnte = String.valueOf(retrieveMdsupDtlInfoObj.get("UsagCpctCnte"));
-                String useAtntMttCnte = String.valueOf(retrieveMdsupDtlInfoObj.get("UseAtntMttCnte"));
-                String cmnTmdcGdncCnte = String.valueOf(retrieveMdsupDtlInfoObj.get("CmnTmdcGdncCnte"));
+                    String RetrieveTreatmentInjectionInformationPersonDetailListStr = RetrieveTreatmentInjectionInformationPersonDetailList.toString();
+                    JSONObject retrieveTreatment = (JSONObject) jsonParser.parse(RetrieveTreatmentInjectionInformationPersonDetailListStr);
+                    retrieveTreatment.get("JinRyoHyungTae");
+                    retrieveTreatment.get("ChoBangHoetSoo");
+                    Object retrieveMdsupDtlInfo = retrieveTreatment.get("RetrieveMdsupDtlInfo");
+                    String retrieveMdsupDtlInfoStr = retrieveMdsupDtlInfo.toString();
+                    JSONObject retrieveMdsupDtlInfoObj = (JSONObject) jsonParser.parse(retrieveMdsupDtlInfoStr);
+                    String drugCode = String.valueOf(retrieveMdsupDtlInfoObj.get("DrugCode"));
+                    String mediPrdcNm = String.valueOf(retrieveMdsupDtlInfoObj.get("MediPrdcNm"));
+                    String cmpnInfo = String.valueOf(retrieveMdsupDtlInfoObj.get("CmpnInfo"));
+                    String tmsgGnlSpcd = String.valueOf(retrieveMdsupDtlInfoObj.get("TmsgGnlSpcd"));
+                    String snglCmtnYn = String.valueOf(retrieveMdsupDtlInfoObj.get("SnglCmtnYn"));
+                    String upsoName = String.valueOf(retrieveMdsupDtlInfoObj.get("UpsoName"));
+                    String upso1 = String.valueOf(retrieveMdsupDtlInfoObj.get("Upso1"));
+                    String fomlCdXplnCnte = String.valueOf(retrieveMdsupDtlInfoObj.get("FomlCdXplnCnte"));
+                    String mdctPathXplnCnte = String.valueOf(retrieveMdsupDtlInfoObj.get("MdctPathXplnCnte"));
+                    String mohwClsfNoXplnCnte = String.valueOf(retrieveMdsupDtlInfoObj.get("MohwClsfNoXplnCnte"));
+                    String atcInfo = String.valueOf(retrieveMdsupDtlInfoObj.get("AtcInfo"));
+                    String kpicInfo = String.valueOf(retrieveMdsupDtlInfoObj.get("KpicInfo"));
+                    String efftEftCnte = String.valueOf(retrieveMdsupDtlInfoObj.get("EfftEftCnte"));
+                    String usagCpctCnte = String.valueOf(retrieveMdsupDtlInfoObj.get("UsagCpctCnte"));
+                    String useAtntMttCnte = String.valueOf(retrieveMdsupDtlInfoObj.get("UseAtntMttCnte"));
+                    String cmnTmdcGdncCnte = String.valueOf(retrieveMdsupDtlInfoObj.get("CmnTmdcGdncCnte"));
 
-                Address address = Address.builder()
-                        .address(name)
-                        .build();
+                    Address address = Address.builder()
+                            .address(name)
+                            .build();
 
-                Diagnosis diagnosisDetails = Diagnosis.builder()
-                        .atcInfo(atcInfo)
-                        .drugCode(drugCode)
-                        .dosage(usagCpctCnte)
-                        .drugName(mediPrdcNm)
-                        .efficacyEffect(efftEftCnte)
-                        .formulation(fomlCdXplnCnte)
-                        .importer(upso1)
-                        .ingredientInfo(cmpnInfo)
-                        .kpicInfo(kpicInfo)
-                        .medicationGuidance(cmnTmdcGdncCnte)
-                        .ministryOfWelfareClassification(mohwClsfNoXplnCnte)
-                        .numberOfPrescriptions(CheobangHoesu)
-                        .precautions(useAtntMttCnte)
-                        .professionalGeneral(tmsgGnlSpcd)
-                        .routeOfAdministration(mdctPathXplnCnte)
-                        .singleCompound(snglCmtnYn)
-                        .typeOfTreatment(JinlyoHyeongtae)
-                        .vendor(upsoName)
-                        .address(address)
-                        .dosingDay(TuyagIlsu)
-                        .visitCount(BangmunIlsu)
-                        .treatmentStartDate(localDateTime)
-                        .user(user).build();
-                diagnosisList.add(diagnosisDetails);
-
-            }List<Diagnosis> diagnosisOptional = diagnosisRepository.findByUserId(user.getId());
-            if(diagnosisList.stream().count()!=diagnosisOptional.stream().count()){
-                List<Diagnosis> diagnoses = diagnosisRepository.saveAll(diagnosisList);
+                    Diagnosis diagnosisDetails = Diagnosis.builder()
+                            .atcInfo(atcInfo)
+                            .drugCode(drugCode)
+                            .dosage(usagCpctCnte)
+                            .drugName(mediPrdcNm)
+                            .efficacyEffect(efftEftCnte)
+                            .formulation(fomlCdXplnCnte)
+                            .importer(upso1)
+                            .ingredientInfo(cmpnInfo)
+                            .kpicInfo(kpicInfo)
+                            .medicationGuidance(cmnTmdcGdncCnte)
+                            .ministryOfWelfareClassification(mohwClsfNoXplnCnte)
+                            .numberOfPrescriptions(CheobangHoesu)
+                            .precautions(useAtntMttCnte)
+                            .professionalGeneral(tmsgGnlSpcd)
+                            .routeOfAdministration(mdctPathXplnCnte)
+                            .singleCompound(snglCmtnYn)
+                            .typeOfTreatment(JinlyoHyeongtae)
+                            .vendor(upsoName)
+                            .address(address)
+                            .dosingDay(TuyagIlsu)
+                            .visitCount(BangmunIlsu)
+                            .treatmentStartDate(localDateTime)
+                            .user(user).build();
+                    diagnosisList.add(diagnosisDetails);
+                }
+                    List<Diagnosis> diagnoses = diagnosisRepository.saveAll(diagnosisList);
             }
         }
         List<Diagnosis> diagnosisList = diagnosisRepository.findByUserId(user.getId());
